e5e0f0ce37a9b05ee50aae9ea5d24f513c5815b9
Adam Houston <phazyy9@gmail.com>
2018-01-21 22:53:35 +0000

    adding script for installing ansible

---
+++ bootstrap.sh
@@ -0,0 +1,20 @@
+if [ ! $(which anisble) ]; then
+  if [ -f /etc/centos-release ] || [ -f /etc/redhat-release ]; then
+    yum install -f epel-release
+    yum makecache fast
+    yum install python-pip ansible
+  elif [ -f /etc/debian_version ] || [ $(grep -qi ubuntu /etc/os-release) ]; then
+    sudo apt-get update
+    sudo apt-get install software-properties-common
+    sudo apt-add-repository ppa:ansible/ansible
+    sudo apt-get update
+    sudo apt-get install -y python-pip ansible
+  else
+    echo 'WARN: Distro not supported by this script..'
+    echo 'See Ansibles docs for manual install steps:'
+    echo 'http://docs.ansible.com/ansible/latest/intro_installation.html'
+  fi
+
+else
+  echo 'INFO: Ansible already installed, exiting..'
+fi
